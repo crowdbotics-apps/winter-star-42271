2ee87c529d3c1b95731eeaf4f5deb56a4be3683e
Crowdbotics <team@crowdbotics.com>
2023-07-19 03:47:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-star-42271",
+  url: "https://winter-star-42271.botics.co",
+  api: "https://winter-star-42271.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
